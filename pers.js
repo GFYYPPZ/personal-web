828ae4f4e0c46dc429564d8915fcf5aeb8c6f4ba
Ward Yi <qy248@cornell.edu>
2024-01-24 23:28:30 -0500

    finish all the section

--- pers.js
+++ pers.js
@@ -7,3 +7,40 @@ $('#about-titles p').click(function() {
     $(this).addClass('active-link');
 })
 
+
+function openmenu() {
+    $('nav ul').css('right','0px');
+}
+
+
+function closemenu() {
+    $('nav ul').css('right','-200px');
+}
+
+
+$(document).scroll(function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $('#backToTop').css('display','block');
+    }
+    else {
+        $('#backToTop').css('display','none')
+    }
+})
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx1dpKlrdkkKt5HrOZmK_PGps9CJD-4NFt58kU5PW8PUXewHGDXgQrObIGA669YyNAaTw/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = 'Message sent successfully';
+        setTimeout(function() {
+            msg.innerHTML = ''
+        }, 5000);
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
